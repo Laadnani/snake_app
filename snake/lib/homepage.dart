9ee4720460418a47aae03f48edb4726dc5bb4d66
Laadnani <laadnani.med@gmail.com>
2024-07-07 16:33:32 +0100

    fixing home page UI
    UI made simple with black and white tone

--- snake/lib/homepage.dart
+++ snake/lib/homepage.dart
@@ -10,29 +10,31 @@ class HomePage extends StatelessWidget {
         body: Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
-      color: Colors.blue,
+      color: const Color.fromARGB(255, 14, 14, 14),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Image.asset('assets/snake_game.png'),
           const SizedBox(height: 50.0),
-          const Text('Welcome to SnakeGameFlutter',
+          const Text('SNAKY',
               style: TextStyle(
                   color: Colors.white,
-                  fontSize: 40.0,
+                  fontSize: 20.0,
                   fontStyle: FontStyle.italic,
                   fontWeight: FontWeight.bold),
               textAlign: TextAlign.center),
           const SizedBox(height: 50.0),
           FilledButton.icon(
+             style: FilledButton.styleFrom(
+              backgroundColor: Color.fromARGB(179, 172, 151, 151)),
              onPressed: () {
                 Navigator.of(context)
                     .push(MaterialPageRoute(builder: (context) => const GamePage()));
               },
               icon: const Icon(Icons.play_circle_filled,
                   color: Colors.white, size: 30.0),
-              label: const Text("Start the Game...",
+              label: const Text("Start",
                   style: TextStyle(color: Colors.white, fontSize: 20.0))),
         ],
       ),
